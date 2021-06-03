a435b2c9495b7c2f6afc424434888d4c84ff5640
Gabriel Grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-06-03 15:15:30 -0300

    configurando variavel de ambiente para analise.host

--- src/main/java/br/com/zupacademy/metricas/geral/ApiDeAnalise.java
+++ src/main/java/br/com/zupacademy/metricas/geral/ApiDeAnalise.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "analise",url = "http://localhost:9999/api")
+@FeignClient(name = "analise",url = "${analise.host}")
 public interface ApiDeAnalise {
 
 	@RequestMapping(method = RequestMethod.POST,value = "/solicitacao")
