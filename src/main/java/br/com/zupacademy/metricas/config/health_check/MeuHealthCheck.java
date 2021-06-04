995374c1f568292dfbaa554a770b86d30bd0d02f
Gabriel Grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-06-04 10:00:10 -0300

    spring actuator

---
+++ src/main/java/br/com/zupacademy/metricas/config/health_check/MeuHealthCheck.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.metricas.config.health_check;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MeuHealthCheck implements HealthIndicator{
+
+	@Override
+    public Health health() {
+        Map<String, String> details = new HashMap<>();
+        details.put("versão", "1.2.3");
+        details.put("descrição", "Meu primeiro Health Check customizado!");
+        
+        return Health.status(Status.UP).withDetails(details).build();
+    }
+}
